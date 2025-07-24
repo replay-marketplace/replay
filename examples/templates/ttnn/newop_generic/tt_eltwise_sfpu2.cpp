3f28f12ff8abe6ceae3beb958f1e8ce9107cba7f
Joe Malone <jmalone@tenstorrent.com>
2025-07-24 11:31:22 -0500

    Fixed template so it actually compiles

--- examples/templates/ttnn/newop_generic/tt_eltwise_sfpu2.cpp
+++ examples/templates/ttnn/newop_generic/tt_eltwise_sfpu2.cpp
@@ -124,7 +124,7 @@ inline void exp_tile_unfolded(uint32_t idst) {
     // THIS IFDEF IS CRITICAL FOR THE TENSTORRENT COMPILER!
     // Only compile this code when building for the math processing thread
 #ifdef TRISC_MATH
-    llk_math_eltwise_unary_sfpu_params<false>(
+    _llk_math_eltwise_unary_sfpu_params_<false>(
         _calculate_exponential_unfolded,
         idst,
         (int)VectorMode::RC,
